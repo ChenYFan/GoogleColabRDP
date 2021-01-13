f19b2f9cd936a4cd74123752fa4c026aface3e5a
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2021-01-13 17:02:17 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+printf "正在进行异步安装 " >&2
+{
+sudo useradd -m CYF
+sudo adduser CYF sudo
+echo 'CYF:1234' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y 
+sudo adduser CYF chrome-remote-desktop
+} &> /dev/null &&
+printf "\n安装完成 " >&2 ||
+printf "\n安装失败 " >&2
+printf '\n请进入https://remotedesktop.google.com/headless 获取liunx密钥并复制在这个地方\n'
+read -p "Paste Here: " CRP
+su - CYF -c """$CRP"""
+printf '完毕！请进入https://remotedesktop.google.com/access/ \n\n'
+if sudo apt-get upgrade &> /dev/null
+then
+    printf "\n\n更新完毕 " >&2
+else
+    printf "\n\n更新失败 " >&2
+fi
